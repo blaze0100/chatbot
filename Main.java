1cf937cd81a73088916471dbcb4c553f9b827a97
blaze0100 <ryancarlosbr001@gmail.com>
2024-06-15 00:53:13 -0300

    criaçao da estrutura do projeto 1,o

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Seja bem vindo ao chatbot, Qual o seu nome?");
+        System.out.println(" Digite seu nome");
+        Scanner sc = new Scanner(System.in);
+        String nome = sc.nextLine();
+        System.out.println("digite sua data de nascimento");
+        int datanasci = sc.nextInt();
+        System.out.println(" Olá " + (nome) + " seja bem vindo novamente, como posso te ajudar? precisa de ajuda?");
+        // ifelse
+
+    }
+
+}
